25f14f8b278e5532c995a5982b4d8ce0208c393e
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-03-11 01:44:53 +0000

    Proyecto Final

--- src/js/app.js
+++ src/js/app.js
@@ -41,16 +41,16 @@ function render(variables = {}) {
           <ul class=${variables.socialMediaPosition}>
             <li><a href=${
               variables.twitter
-            }><i class="fab fa-twitter"></i></a></li>
+            } target="_blank"><i class="fab fa-twitter"></i></a></li>
             <li><a href=${
               variables.github
-            }><i class="fab fa-github"></i></a></li>
+            } target="_blank"><i class="fab fa-github"></i></a></li>
             <li><a href=${
               variables.linkedin
-            }><i class="fab fa-linkedin"></i></a></li>
+            } target="_blank"><i class="fab fa-linkedin"></i></a></li>
             <li><a href=${
               variables.instagram
-            }><i class="fab fa-instagram"></i></a></li>
+            } target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
